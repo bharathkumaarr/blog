8fd7cb526af0b5428f77b0e60a1bd1c458633465
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-07-08 00:12:26 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
-
-import { Button } from "@/components/ui/button";
+import { Routes, Route, Link } from "react-router-dom";
 import  Login from "./pages/Login";
 import  Register from "./pages/Register";
+
+
 function App() {
 
 
   return (
-    // <Login />
-    <Register />
+    <div className="p-4">
+      <nav className="mb-4 space-x-4">
+        <Link to='/login' className="text-blue-600 underline">Login</Link>
+        <Link to='/register' className="text-blue-600 underline">Register</Link>
+      </nav>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
 
   )
 }
