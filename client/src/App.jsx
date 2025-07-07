c6519deca594ce72127590694cfdcf96f048aa40
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-07-08 00:05:03 +0530

    shacn comp

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import { Button } from "@/components/ui/button";
+import  Login from "./pages/Login";
+import  Register from "./pages/Register";
+function App() {
+
+
+  return (
+    // <Login />
+    <Register />
+
+  )
+}
+
+export default App
