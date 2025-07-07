b96fbd8cb7a015334fbc1b0328d56579e1f52f85
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-07-08 00:50:23 +0530

    posts routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 import { Routes, Route, Link } from "react-router-dom";
 import  Login from "./pages/Login";
 import  Register from "./pages/Register";
+import Posts from "./pages/Posts";
+import PostDetail from "./pages/PostDetail";
+
+
 
 
 function App() {
@@ -11,10 +15,13 @@ function App() {
       <nav className="mb-4 space-x-4">
         <Link to='/login' className="text-blue-600 underline">Login</Link>
         <Link to='/register' className="text-blue-600 underline">Register</Link>
+        <Link to="/posts" className="text-blue-600 underline">Posts</Link>
       </nav>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/post/:id" element={<PostDetail />} />
       </Routes>
     </div>
 
