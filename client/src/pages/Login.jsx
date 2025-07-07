5e3984e8d0c1244c4ad71f593acca3f2218d79da
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-07-08 02:54:01 +0530

    update API endpoints to use deployed Render backend URL

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
 
     const handleLogin = async() =>  {
         try {
-            const res = await fetch("${import.meta.env.VITE_API_BASE_URL}/api/auth/login", {
+            const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/auth/login`, {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify({email, password}),
